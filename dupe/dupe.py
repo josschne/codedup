bf6c10ff315e37ec458d34d93ef1f567bf9e31d6
Joe <josschne@gmail.com>
2013-10-28 23:21:53 -0500

    Transitioned to naive md5 algorithm

--- dupe/dupe.py
+++ dupe/dupe.py
@@ -1,6 +1,20 @@
-
-
+from hashlib import md5
 
 class dupe:
+
+	def getMD5TwoGrams(self, inputString):
+		md5_two_grams = []
+		lines = inputString.split()
+		for i in xrange(0, len(lines)-1):
+			line_hash = md5()
+			line_hash.update(lines[i])
+			line_hash.update(lines[i+1])
+			md5_two_grams.append(line_hash.digest())
+		return md5_two_grams
+
 	def dupe(self, first, second):
-		return first == second
+		first_md5s = self.getMD5TwoGrams(first)
+		second_md5s = self.getMD5TwoGrams(second)
+
+		matches = list(set(first_md5s) & set(second_md5s))
+		return bool(matches)
--- test/testDupe.py
+++ test/testDupe.py
@@ -7,9 +7,17 @@ class testBasicMatching(unittest.TestCase):
 		self.my_dupe = dupe.dupe()
 
 	def testTwoDifferentLinesAreNotDuplicates(self):
-		result = self.my_dupe.dupe('Hello', 'World')
+		result = self.my_dupe.dupe('Hello\nHello\n', 'World\nWorld\n')
 		self.assertEqual(result, False)
 
 	def testTwoEqualLinesAreDuplicates(self):
-		result = self.my_dupe.dupe('Hello', 'Hello')
+		result = self.my_dupe.dupe('Hello\nWorld\n', 'Hello\nWorld\n')
+		self.assertEqual(result, True)
+
+	def testFindDuplicateInMultilineSource(self):
+		result = self.my_dupe.dupe('Brave\nNew\nWorld\n', 'New\nWorld\n')
+		self.assertEqual(result, True)
+
+	def testFindDuplicateInMultilineSourceAndTarget(self):
+		result = self.my_dupe.dupe('Brave\nNew\nWorld\nPeace\n', 'World\nPeace\n')
 		self.assertEqual(result, True)
